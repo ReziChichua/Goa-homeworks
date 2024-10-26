82a79585c6ccd87bc2fb657c1a2bab27bebda0ac
ReziChichua <rezichichua19@gmail.com>
2024-10-26 22:22:12 +0400

    first commit

---
+++ Day-01/Homework/day1.py
@@ -0,0 +1,13 @@
+name = ("rezi ")
+surname = ("chichua ")
+hobby = ("chemi hobi airs csgos tamashi da programireba ")
+country = ("me saqartvelodan var")
+
+
+print("chemi saxelia "+ name + surname + hobby+ "da " + country)
+
+print(5+5)
+print(12+35)
+print(500//3)
+print(22**2)
+print(240-60)
