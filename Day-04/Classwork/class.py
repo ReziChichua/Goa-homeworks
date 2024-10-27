4ee97d42707d8b7a6ba06a21d58f121c103c3b34
ReziChichua <rezichichua19@gmail.com>
2024-10-27 19:19:29 +0400

    second commit

---
+++ Day-04/Classwork/class.py
@@ -0,0 +1,38 @@
+#1 ცვლადის სახელის შერჩევა
+#2 უდრის ნიშანი
+#3 ცვლადის მნიშვნელობის მინიჭება
+
+
+num1 =5
+num2 = 10
+sum = num1+num2
+print(sum)
+
+price = 50
+price = 80
+print(price)
+
+
+
+print("Hello World!")
+
+greeting = "good morning"
+print(greeting)
+
+first_name = "Alice "
+last_name = "Smith"
+print("Name: " + first_name + last_name)
+
+number = "50"
+print("The number is: " + number)
+
+name = "George"
+print(name)
+
+age = "5"
+print("I am " + age + " years old")
+
+num1 = 10
+num2 = 20
+result = num1+num2
+print(result)
---
+++ Day-04/Homework/day4.py
@@ -0,0 +1,22 @@
+points = 50
+
+points = 100
+ 
+points = 150
+
+points = 200
+
+print(points)
+
+age = 156
+
+print(age)
+
+x = 10
+
+y = 2
+
+print(x+y)
+print(x-y)
+print(x*y)
+print(x//y)
