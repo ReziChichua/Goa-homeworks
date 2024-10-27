ae395bd085a6651f1df5c95966dab16cfa450d4c
ReziChichua <rezichichua19@gmail.com>
2024-10-27 16:05:22 +0400

    second commit

---
+++ Day-02/Homework/day2.py
@@ -0,0 +1,11 @@
+print("Rezi Chichua")
+
+print("Goa aris sauketeso programirebis skola")
+
+print(10+20)
+
+num1= "10"
+num2= "20"
+print(num1 + "+" + num2 + "=" + str(int(num1) + int(num2)))
+print("20+40")
+print("20+40=60")
---
+++ Day-03/Classwork/class.py
@@ -0,0 +1,37 @@
+print(type(5))
+
+#int-string
+#str-integer
+#float
+#boolean
+
+#string'ი არის ნებისმიერი სიმბოლოების ერთობლიობარომელსაც გარშემო აქვს ბრჭყალები
+# მაგალიტად
+ #"abcd"
+ #"12345"
+ #"./,p;123"
+ #'abcd
+
+print('isn"t')
+print("isn't")
+
+#integer ქართულად ნიშნავს მთელ რიცხვს
+
+#float ქართულად ნიშნავს წილად რიცხვს
+
+print(type(47.0))
+print(type(47))
+print(47.0+1)
+print(47+1)
+
+#boolean არის მონაცემთა ტიპი, რომელსაც აქვს მხოლოდ 2 შესაძლო მნიშვლენობა
+#ესენი არის: True False
+
+#True - 1
+#False - 0
+
+name = "rezi"
+print(name)
+
+x=5
+print(x+5)
