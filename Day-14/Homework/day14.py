13aeb184658f746f075f7136076e18b745cf8d9c
ReziChichua <rezichichua19@gmail.com>
2024-11-04 23:56:43 +0400

    second commit

---
+++ Day-14/Homework/day14.py
@@ -0,0 +1,23 @@
+count = 0
+while count < 5:
+    print("Hello World!")
+    count = count + 1
+
+
+for i in range (10):
+    print("Hello World!")
+
+
+for n in range(100):
+    print(n)
+
+
+for number in range(49, 51):
+    print (number)
+
+
+x=16
+
+while x < 57:
+    print(x)
+    x= x+1
