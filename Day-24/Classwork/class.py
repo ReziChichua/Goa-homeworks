2bca6a6343ea19242a6f07ec768fc86649008e7e
ReziChichua <rezichichua19@gmail.com>
2024-11-16 21:49:27 +0400

    second commit

---
+++ Day-24/Classwork/class.py
@@ -0,0 +1,18 @@
+print(len("Hello"))
+list1=[0, 1, 2, 3, 4]
+list1.append(5)
+print(list1)
+
+list1.insert(0, "hello")
+print(list1)
+
+list2=["python", "html", "css", "javascript"]
+list2.pop(2)
+print(list2)
+
+#pop-ის დროს უნდა მივუთითოთ ინდექსი
+#remove-ის დროს უნდა მივუთითოთ ელემენტი
+
+list3=["python", "html", "css", "javascript"]
+list3.insert(2, "C++")
+print(list3)
---
+++ Day-24/Homework/day23.py
@@ -0,0 +1,22 @@
+my_list1 = [1, 2, 3, 4, 5, 6, 7, 8]
+my_list1.pop(5)
+print(my_list1)
+
+my_list2 = [10, 20, 30, 40, 50]
+my_list2.remove(20)
+print(my_list2)
+
+my_list3 = ['a', 'b', 'c']
+my_list3.append('d')
+print(my_list3)
+
+my_list4 = [100, 200, 300]
+my_list4.insert(1, 150)
+print(my_list4)
+
+my_list5 = ['apple', 'banana', 'cherry']
+my_list5.pop()
+print(my_list5)
+
+my_list6 = [1, 2, 3, 4, 5]
+element_count = len(my_list6)
