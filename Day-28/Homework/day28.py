d9863606988d3e56bdce0315a2e1c4ee59304104
ReziChichua <rezichichua19@gmail.com>
2024-11-23 00:25:37 +0400

    second commit

--- Day-28/Homework/day28.py
+++ Day-28/Homework/day28.py
@@ -1,3 +1,25 @@
-x = 0
-for i in range(1, x + 1):
+#1)
+def count_sheeps(sheeps):
+    count= 0
+    for sheep in sheeps:
+        if sheep:
+            count += 1    
+    return count
 
+
+#2)
+def no_space(x):
+    return x.replace(' ' ,'')
+
+#3)
+def double_integer(i):
+    return i*2
+
+#4)
+def greet(name):
+    return "Hello, " + name + " how are you doing today?"
+
+
+#5)
+def boolean_to_string(b):
+    return str(b)
