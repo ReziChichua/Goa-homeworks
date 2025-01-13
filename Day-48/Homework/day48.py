d69819925b8d75dc4ccef940d69e04b9526b33f1
ReziChichua <rezichichua19@gmail.com>
2025-01-13 19:44:56 +0400

    second commit

---
+++ Day-48/Homework/day48.py
@@ -0,0 +1,21 @@
+#1
+def hoop_count(n):
+          return "Great, now move on to tricks" if n >= 10 else "Keep at it until you get it"
+
+#2
+def cockroach_speed(s):
+    return int(s * 100000 / 3600)
+
+#3
+def switch_it_up(number):
+    number_words = ["Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"]
+    
+    return number_words[number]
+
+#4
+def square(n):
+    return n ** 2
+
+#5
+def remove_every_other(my_list):
+     return my_list[::2]
